bfcd0fcf49d97f5af1f952f6d81a1c7088ffb391
Gareth Luckett <gareth.luckett@gmail.com>
2019-09-25 11:48:17 +0100

    Added chocolateyInstall script

---
+++ tools/chocolateyInstall.ps1
@@ -0,0 +1,10 @@
+$packageName    = 'podman-cli'
+$url            = 'https://github.com/lareeth/podman-cli-builder/releases/download/v1.6.0-rc1/podman-remote-v1.6.0-windows.zip'
+$validExitCodes = @(0)
+ 
+$toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+ 
+Install-ChocolateyZipPackage `
+  -PackageName $packageName `
+  -Url64bit "$url" `
+  -UnzipLocation "$toolsDir"
