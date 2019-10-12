b2f62a844681b4ceaa1cdba3d139f3129dcfa0e4
Gareth Luckett <gareth.luckett@gmail.com>
2019-10-12 16:56:33 +0100

    Updated to 1.6.1-rc1

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 $packageName    = 'podman-cli'
-$url            = 'https://storage.cloud.google.com/libpod-master-releases/podman-remote-v1.6.0-28-gdac7889d-master-windows-amd64.zip?authuser=0'
+$url            = 'https://storage.cloud.google.com/libpod-master-releases/podman-remote-v1.6.1-rc1-9-g960f07b0-master-windows-amd64.zip?authuser=0'
 $validExitCodes = @(0)
  
 $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
