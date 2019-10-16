115d54efff3c287633a31a9f5918b1c2eba7fea4
Gareth Luckett <gareth.luckett@gmail.com>
2019-10-16 01:59:58 +0100

    Switched back to lareeth/podman-cli-builder builds

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 $packageName    = 'podman-cli'
-$url            = 'https://storage.cloud.google.com/libpod-master-releases/podman-remote-v1.6.0-28-gdac7889d-master-windows-amd64.zip?authuser=0'
+$url            = 'https://github.com/lareeth/podman-cli-builder/releases/download/v1.6.0/podman-remote-v1.6.0-windows.zip'
 $validExitCodes = @(0)
  
 $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
