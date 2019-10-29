fc29b25a1c202e5d0e1647a245a9736ba02fef65
Gareth Luckett <gareth.luckett@gmail.com>
2019-10-29 17:28:46 +0000

    Update podman to 1.6.2

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 $packageName    = 'podman-cli'
-$url            = 'https://github.com/lareeth/podman-cli-builder/releases/download/v1.6.1-rc1/podman-remote-v1.6.1-windows.zip'
-$checksum       = '02d77aba6f8c265600ad11bb13b7fa137af8dd57b0ff1c1b481b1684ec635312'
+$url            = 'https://github.com/lareeth/podman-cli-builder/releases/download/v1.6.2/podman-remote-v1.6.2-windows.zip'
+$checksum       = '59ab5fe8917ba1bb725c503282229aeb065af8de1ddb5259652693cb3c28fca5'
 $checksumType   = 'sha256'
 $validExitCodes = @(0)
  
