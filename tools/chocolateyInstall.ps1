69e3aa6af11ae791340e65d6f061a3b8c897ebf6
Gareth Luckett <gareth.luckett@gmail.com>
2023-05-31 13:40:59 +0100

    Update to podman 2.0.4

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 $packageName    = 'podman-cli'
-$url            = 'https://github.com/lareeth/podman-cli-builder/releases/download/v2.0.3/podman-remote-release-windows.zip'
-$checksum       = '720B56E715961A1B870C04E30A23EAE70ED066BF1DA33DADD319BEC2E3D9FC0A'
+$url            = 'https://github.com/lareeth/podman-cli-builder/releases/download/v2.0.4/podman-remote-release-windows.zip'
+$checksum       = '3B2431BABCEBF8FED38E2665261A0FAEA7347149804ABFADEAEC397F11E31EB4'
 $checksumType   = 'sha256'
 $validExitCodes = @(0)
  
