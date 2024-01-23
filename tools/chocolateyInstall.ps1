647d6535abe3c413210f7224f68ec042e92aab4e
Gareth Luckett <gareth.luckett@gmail.com>
2024-01-23 13:36:01 +0000

    Update podman to 4.8.3

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 $packageName    = 'podman-cli'
-$url            = 'https://github.com/containers/podman/releases/download/v4.8.2/podman-remote-release-windows_amd64.zip'
-$checksum       = '9195be198f7a3a53e5e9fb6c43b7105d1e8440da9372da20fb6cabaa330f0766'
+$url            = 'https://github.com/containers/podman/releases/download/v4.8.3/podman-remote-release-windows_amd64.zip'
+$checksum       = '80d06960ef05f8102b9f8c4f59ad2fa41e360ae9dcf003bc0f002de2e8f0c8b8'
 $checksumType   = 'sha256'
 $validExitCodes = @(0)
  
