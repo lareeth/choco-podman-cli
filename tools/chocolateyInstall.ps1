c47a34b9e6cc7263a226438f4632d0e1625322ee
David Gardiner <david@gardinerfamily.au>
2026-05-11 10:14:28 +0930

    5.8.2

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$packageName    = 'podman-cli'
-$url_amd64      = 'https://github.com/containers/podman/releases/download/v5.8.0/podman-remote-release-windows_amd64.zip'
-$url_arm64      = 'https://github.com/containers/podman/releases/download/v5.8.0/podman-remote-release-windows_arm64.zip'
-$checksum_amd64 = '5d88d730f9963c3cfabae4c8e688a025f59cd5ff045878506e3955b004eeb522'
-$checksum_arm64 = '43608836a0e9977acc0316dbd180fef68779d308aff21bd34ae91f01e9984041'
+$url_amd64      = 'https://github.com/containers/podman/releases/download/v5.8.2/podman-remote-release-windows_amd64.zip'
+$url_arm64      = 'https://github.com/containers/podman/releases/download/v5.8.2/podman-remote-release-windows_arm64.zip'
+$checksum_amd64 = '1b60aae4bd4879c3932c283d2070bb87116ec1f0ab3a4a98eff7e65f318f9e4e'
+$checksum_arm64 = '06c64d11267232ba21d3e43962c0e9036f24550d87fb811bc995195d82c32fca'
 $checksumType_amd64   = 'sha256'
 $checksumType_arm64   = 'sha256'
  
