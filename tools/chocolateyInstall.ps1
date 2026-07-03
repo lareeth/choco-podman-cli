98bcb96ce91d587011da1ed40cdc1e3c8615bde5
Gareth Luckett <gareth.luckett@gmail.com>
2026-07-03 12:57:36 +0100

    Updated to 5.8.2

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$packageName    = 'podman-cli'
-$url_amd64      = 'https://github.com/containers/podman/releases/download/v5.8.1/podman-remote-release-windows_amd64.zip'
-$url_arm64      = 'https://github.com/containers/podman/releases/download/v5.8.1/podman-remote-release-windows_arm64.zip'
-$checksum_amd64 = '1c0255c38982a1494205bde6f6e5cafcfdca25a64b9fd36510bc45e7c32495b7'
-$checksum_arm64 = '6b8794086ac15da544dba156056ac0a7364eb12fa94edc26125ad681229a1138'
+$url_amd64      = 'https://github.com/containers/podman/releases/download/v5.8.2/podman-remote-release-windows_amd64.zip'
+$url_arm64      = 'https://github.com/containers/podman/releases/download/v5.8.2/podman-remote-release-windows_arm64.zip'
+$checksum_amd64 = '1b60aae4bd4879c3932c283d2070bb87116ec1f0ab3a4a98eff7e65f318f9e4e'
+$checksum_arm64 = '06c64d11267232ba21d3e43962c0e9036f24550d87fb811bc995195d82c32fca'
 $checksumType   = 'sha256'
 $validExitCodes = @(0)
  
